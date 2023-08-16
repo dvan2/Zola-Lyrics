dd7ed3028fdbe04f5ecec1d7325156de03c45799
David <davvan@pdx.edu>
2023-08-16 13:18:20 -0700

    Second commit

---
+++ app/src/main/java/com/example/lyrics/ui/home/HomeFragment.kt
@@ -0,0 +1,52 @@
+package com.example.lyrics.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.lyrics.databinding.FragmentHomeBinding
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStream
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        binding.readBtn.setOnClickListener {
+
+
+
+
+            val inputStream: InputStream? = context?.assets?.open("nongtel3.txt")
+                ?: null
+            val text = inputStream?.bufferedReader().use { it.readText() }
+
+            Toast.makeText(activity, text, Toast.LENGTH_SHORT).show()
+
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
